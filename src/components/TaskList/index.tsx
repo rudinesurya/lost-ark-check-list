43a1cd85984df8628b4cab3973a18e8910bcd1bc
Rudine Hartanto <rudine.hartanto@intellisense.io>
2025-01-12 01:00:04 +0800

    update

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -7,8 +7,8 @@ const TaskList: React.FC<TaskListProps> = ({ tasks, taskCompletion, onTaskToggle
             {tasks.map((task, index) => (
                 <Checkbox
                     key={index}
-                    checked={taskCompletion[index]}
-                    onChange={() => onTaskToggle(index)}
+                    checked={taskCompletion[task] || false}
+                    onChange={() => onTaskToggle(task)}
                     label={task}
                 />
             ))}
@@ -19,8 +19,8 @@ const TaskList: React.FC<TaskListProps> = ({ tasks, taskCompletion, onTaskToggle
 // Add PropTypes for type checking
 interface TaskListProps {
     tasks: string[]; // Array of task names
-    taskCompletion: boolean[]; // Array of task completion statuses
-    onTaskToggle: (taskIndex: number) => void; // Function to handle toggling tasks
+    taskCompletion: { [key: string]: boolean };
+    onTaskToggle: (taskName: string) => void; // Function to handle toggling tasks
 }
 
 export default TaskList;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,12 +4,17 @@ import TaskList from '../../components/TaskList';
 
 const appName = 'lost-ark-check-list';
 
+// UTC
+const weeklyResetDay = 3; // Wednesday
+const resetHours = 10;
+
 import jsonData from '../../../template.json' assert { type: 'json' };
 
 export default function App() {
   const [taskCompletionState, setTaskCompletionState] = useState({});
   const [loading, setLoading] = useState(true); // Track loading state
   const isInitialRender = useRef(true); // Track initial render
+  const [checkResetTimer, setCheckResetTimer] = useState(false);
 
   // Load initial state from localStorage
   useEffect(() => {
@@ -22,31 +27,115 @@ export default function App() {
         console.error("Failed to parse taskCompletionState from localStorage:", error);
       }
       const initialState = jsonData.reduce((acc, char) => {
-        acc[char.name] = savedState[char.name] || char.tasks.map(() => false);
+        acc[char.name] = savedState[char.name] ||
+          char.tasks.reduce((taskAcc, task) => {
+            taskAcc[task] = false; // Initialize each task as false
+            return taskAcc;
+          }, {});
         return acc;
       }, {});
+
       setTaskCompletionState(initialState);
       setLoading(false); // Data loaded
     };
 
     loadState();
+    setCheckResetTimer(true);
   }, []);
+
+  useEffect(() => {
+    if (checkResetTimer) {
+      // const testDate = "2025-01-11T09:00:00Z";
+      const currentDateTimeUTC = new Date();
+      const lastCheckedDateStr = localStorage.getItem(`${appName}-lastCheckedDate`);
+      const lastCheckedDate = lastCheckedDateStr ? new Date(lastCheckedDateStr) : null;
+
+      const currentDateAtResetHour = new Date(currentDateTimeUTC);
+      currentDateAtResetHour.setUTCHours(resetHours, 0, 0, 0); // Set the hour to the reset Hour
+
+      // Check if lastCheckedDate is before the reset hour and currentDateTimeUTC is after the reset hour
+      if (lastCheckedDate && lastCheckedDate < currentDateAtResetHour && currentDateTimeUTC > currentDateAtResetHour) {
+        if (isWeeklyResetDue(lastCheckedDate, currentDateTimeUTC)) {
+          console.log("Weekly reset !");
+          handleWeekyAutoResetTasks();
+        } else {
+          console.log("Daily reset !");
+          handleDailyAutoResetTasks();
+        }
+        localStorage.setItem(`${appName}-lastCheckedDate`, currentDateTimeUTC.toISOString());
+      } else {
+        console.log("check passed !");
+        console.log(`lastCheckedDate= ${lastCheckedDate.toUTCString()}`);
+        console.log(`past daily reset= ${currentDateAtResetHour.toUTCString()}`);
+      }
+    }
+
+  }, [checkResetTimer]);
+
+  // Function to check if the weekly reset is due (on Wednesday at 10 AM UTC)
+  const isWeeklyResetDue = (lastCheckedDate, currentDateTimeUTC) => {
+    // Get the current date at the reset hour (Wednesday 10 AM UTC)
+    const currentDateAtResetHour = new Date(currentDateTimeUTC);
+    currentDateAtResetHour.setUTCHours(resetHours, 0, 0, 0); // Set to 10 AM UTC
+
+    // Adjust the currentDate to the most recent Wednesday before or equal to today (if today is not Wednesday)
+    const currentWeekday = currentDateTimeUTC.getUTCDay();
+    const daysUntilLastWednesday = (currentWeekday + (7 - weeklyResetDay)) % 7;  // Calculate how many days since last Wednesday
+    const lastWednesdayDate = new Date(currentDateTimeUTC);
+    lastWednesdayDate.setUTCDate(currentDateTimeUTC.getUTCDate() - daysUntilLastWednesday);
+    lastWednesdayDate.setUTCHours(resetHours, 0, 0, 0); // Set it to Wednesday at 10 AM UTC
+
+    // Check if a full week has passed since the last checked date
+    if (lastCheckedDate && lastCheckedDate < lastWednesdayDate && currentDateTimeUTC > lastWednesdayDate) {
+      return true;  // Weekly reset is due
+    }
+
+    return false; // Weekly reset is not due
+  };
 
   // Save updated state to localStorage whenever taskCompletionState changes
   useEffect(() => {
     if (isInitialRender.current) {
       isInitialRender.current = false; // Set flag to false after initial render
     } else if (!loading) {
+      console.log(taskCompletionState["Pheoneater"]);
       localStorage.setItem(`${appName}-taskCompletionState`, JSON.stringify(taskCompletionState));
     }
   }, [taskCompletionState, loading]);
 
   // Handler to update task completion state
-  const handleTaskToggle = (characterName, taskIndex) => {
-    // console.log(`${characterName} --- ${taskIndex}`);
+  const handleTaskToggle = (characterName, taskName) => {
     const updatedState = { ...taskCompletionState };
-    updatedState[characterName][taskIndex] = !updatedState[characterName][taskIndex];
+
+    // Toggle the completion state of the specified task
+    updatedState[characterName] = {
+      ...updatedState[characterName], // Copy existing tasks for the character
+      [taskName]: !updatedState[characterName][taskName] // Toggle the specific task
+    };
     setTaskCompletionState(updatedState);
+  };
+
+  // Handler to reset all tasks to false
+  const handleDailyAutoResetTasks = () => {
+    window.alert("Daily reset has happened.");
+    const resetState = jsonData.reduce((acc, char) => {
+      acc[char.name] = {
+        ...taskCompletionState[char.name],  // Retain other fields if present
+        "World Event": false // Reset "World Event" field to false
+      };
+      return acc;
+    }, {});
+    setTaskCompletionState(resetState);
+  };
+
+  // Handler to reset all tasks to false
+  const handleWeekyAutoResetTasks = () => {
+    window.alert("Weekly reset has happened.");
+    const resetState = jsonData.reduce((acc, char) => {
+      acc[char.name] = {};
+      return acc;
+    }, {});
+    setTaskCompletionState(resetState);
   };
 
   // Handler to reset all tasks to false
@@ -54,9 +143,10 @@ export default function App() {
     const confirmed = window.confirm("Are you sure you want to reset all tasks?");
     if (confirmed) {
       const resetState = jsonData.reduce((acc, char) => {
-        acc[char.name] = char.tasks.map(() => false);
+        acc[char.name] = {};
         return acc;
       }, {});
+
       setTaskCompletionState(resetState);
     }
   };
@@ -82,8 +172,8 @@ export default function App() {
                 <Header as="h5">{character.name}</Header>
                 <TaskList
                   tasks={character.tasks}
-                  taskCompletion={taskCompletionState[character.name] || []}
-                  onTaskToggle={(taskIndex) => handleTaskToggle(character.name, taskIndex)} />
+                  taskCompletion={taskCompletionState[character.name] || {}}
+                  onTaskToggle={(taskName) => handleTaskToggle(character.name, taskName)} />
               </Card.Content>
             </Card>
           </Grid.Column>
